c8814d4a56b3be5bc843047d6c7eb7daa902fe3b
Martins-O <jojololamartins686@gmail.com>
2023-01-15 00:42:51 +0100

    update

---
+++ src/map/CountOccurrenceOfWords.java
@@ -0,0 +1,26 @@
+package map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CountOccurrenceOfWords {
+	public static void main(String[] args) {
+		String text = "Good morning. Have a good class. " + "Have a good visit. Have fun!";
+		Map<String, Integer> map = new TreeMap<>();
+		String[] words = text.split("[\\s+\\p{P}]");
+		for(int i = 0; i < words.length; i++) {
+			String key = words[i].toLowerCase();
+			if(key.length() > 0) {
+				if(! map.containsKey(key)) {
+					map.put(key, 1);
+				} else {
+					int value = map.get(key);
+					value++;
+					map.put(key, value);
+				}
+			}
+		}
+		map.forEach((key, value) -> System.out.println(key + "\t" + value));
+	}
+}
+

a689a2a0c40afd470b27c922743e8def63518c67
Martins-O <jojololamartins686@gmail.com>
2023-01-14 23:34:19 +0100

    first commit

---
+++ src/set/LinkedHashSets.java
@@ -0,0 +1,10 @@
+package set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSets {
+	public static void main(String[] args) {
+		Set<String> set = new LinkedHashSet<>();
+	}
+}
